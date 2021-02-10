d7de25692a6df65baef6e257b12513d1c1c190f1
Anshuman Ranjan <500062237@stu.upes.ac.in>
2021-02-10 15:15:35 +0530

    Create joi_implementation.js

---
+++ Mongoose_notes/joi_implementation.js
@@ -0,0 +1,50 @@
+const Joi = require('joi');
+
+// Reference code from https://joi.dev/api/?v=17.4.0
+
+//Either use the desired imported schema for Joi or use the given code in the schema file itself.
+//Preferred way of doing is the first one.
+
+const schema = Joi.object({
+    username: Joi.string()
+        .alphanum()
+        .min(3)
+        .max(30)
+        .required(),
+
+    password: Joi.string()
+        .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+
+    repeat_password: Joi.ref('password'),
+
+    access_token: [
+        Joi.string(),
+        Joi.number()
+    ],
+
+    birth_year: Joi.number()
+        .integer()
+        .min(1900)
+        .max(2013),
+
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+})
+    .with('username', 'birth_year')
+    .xor('password', 'access_token')
+    .with('password', 'repeat_password');
+
+
+schema.validate({ username: 'abc', birth_year: 1994 });
+// -> { value: { username: 'abc', birth_year: 1994 } }
+
+schema.validate({});
+// -> { value: {}, error: '"username" is required' }
+
+// Also -
+
+//Try to use this validation method whenever applicable.
+try {
+    const value = await schema.validateAsync({ username: 'abc', birth_year: 1994 });
+}
+catch (err) { }

d01f3ce1db5b3f114087c024a3fb0c54df7aa578
Anshuman Ranjan <500062237@stu.upes.ac.in>
2021-05-19 14:17:22 +0530

    Update delete_query_in_mongoose.js

--- Mongoose_notes/delete_query_in_mongoose.js
+++ Mongoose_notes/delete_query_in_mongoose.js
@@ -1,7 +1,7 @@
 const deleteDocument = async (_id)=>{
    try{
-    // const result = await new Playlist.deleteOne(_id);
-    const result = await new Playlist.findByIdAndDelete(_id);
+    // const result = await new Playlist.deleteOne({_id : id});
+    const result = await new Playlist.findByIdAndDelete(id);
     console.log(result);
    }catch(err){
        console.log(err);
